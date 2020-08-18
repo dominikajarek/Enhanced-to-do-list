6892f4bbc1803bfff158c3b9f3efed9f0b2a3add
Mat191126 <matprz11@gmail.com>
2020-08-18 12:49:04 +0200

    add cloning function

---
+++ js/mainPage.js
@@ -0,0 +1,29 @@
+let taskNumber = 0;
+
+(() => {
+    document.getElementById("add-task").addEventListener('click', function () {
+        addNewTask();
+    });
+})();
+
+function addNewTask() {
+    const addNewTask = function () {
+        taskNumber++;
+        let taskId = "task_" + taskNumber;
+
+        const template = document.querySelector('#task-template');
+        const clonedTemplate = document.importNode(template.content, true);
+
+        clonedTemplate.querySelector('.task').setAttribute('id', taskId);
+
+        clonedTemplate.getElementById("remove-task").addEventListener('click', function () {
+            this.parentElement.remove();
+        })
+
+        return clonedTemplate;
+    }
+
+    const newTask = addNewTask();
+    console.log(newTask);
+    document.getElementById("tasks").appendChild(newTask);
+}
